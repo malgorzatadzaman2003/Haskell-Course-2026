c23dc273d4349295720ab5edb25f6afe2cf6e7b2
Małgorzata Dżaman <malgorzatadzaman@MacBook-Pro-Magorzata.local>
2026-03-24 15:57:07 +0100

    Ex 2: Add seqToList and seqLength

--- homeworks/hw2/Solution.hs
+++ homeworks/hw2/Solution.hs
@@ -1,3 +1,5 @@
+import Data.Foldable (toList) -- for seqToList
+
 -- Exercise 1: Functor for Sequence
 
 data Sequence a = Empty | Single a | Append (Sequence a) (Sequence a)
@@ -11,6 +13,18 @@ instance Functor Sequence where
 -- Exercise 2: Foldable for Sequence
 
 instance Foldable Sequence where
+    foldMap :: Monoid m => (a -> m) -> Sequence a -> m
     foldMap _ Empty = mempty
     foldMap f (Single x) = f x
     foldMap f (Append l r) = foldMap f l <> foldMap f r
+
+-- seqToList
+
+seqToList :: Sequence a -> [a]
+seqToList = toList
+
+-- seqLength
+
+seqLength :: Sequence a -> Int
+seqLength = length
+
